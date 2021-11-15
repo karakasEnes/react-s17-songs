1be14eafd75194d2f23dd0e87423e3f837da63f6
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-15 11:35:09 +0300

    mapStateToProps configurate

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
 
 const songsReducer = () => {
-  return [];
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All Star", duration: "3:15" },
+    { title: "I want it That Way", duration: "1:03" },
+  ];
 };
 
 const selectedSongReducer = (selectedSong = null, action) => {
